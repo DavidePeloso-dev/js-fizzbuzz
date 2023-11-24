0140bb1c0e85bdfbdb2e0b21b54ff25aff6be994
DavidePeloso-dev <davide.peloso.e@gmail.com>
2023-11-24 11:34:20 +0100

    Add version with append()

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,60 @@ for(let i = 1; i <= 100; i++){
     
 
 }
+
+const rowElAp = document.querySelector('.ap-row')
+for(let i = 101; i <= 200; i++){
+    if(i % 5 == 0 && i % 3 == 0){
+        let i = 'FizzBuzz'
+        //const colMarkUp = `<div class='col col-lg-2 col-xl py-3'><div class='box bg-fizzbuzz'>${i}</div></div>`;
+        const colEL = document.createElement('div');
+        const boxEl = document.createElement('div');
+        colEL.classList = 'col col-lg-2 col-xl py-3';
+        boxEl.classList = 'box bg-fizzbuzz';
+        boxEl.innerHTML = i;
+        colEL.append(boxEl);
+        console.log(boxEl);
+        rowElAp.append(colEL);
+        console.log(i);
+
+    } else if(i % 3 == 0){
+        let i = 'Fizz';
+        //const colMarkUp = `<div class='col col-lg-2 col-xl py-3'><div class='box bg-fizz'>${i}</div></div>`;
+        const colEL = document.createElement('div');
+        const boxEl = document.createElement('div');
+        colEL.classList = 'col col-lg-2 col-xl py-3';
+        boxEl.classList = 'box bg-fizz';
+        boxEl.innerHTML = i;
+        colEL.append(boxEl);
+        console.log(boxEl);
+        rowElAp.append(colEL);
+        console.log(i);
+
+    } else if(i % 5 == 0){
+        let i = 'Buzz';
+        //const colMarkUp = `<div class='col col-lg-2 col-xl py-3'><div class='box bg-buzz'>${i}</div></div>`;
+        const colEL = document.createElement('div');
+        const boxEl = document.createElement('div');
+        colEL.classList = 'col col-lg-2 col-xl py-3';
+        boxEl.classList = 'box bg-buzz';
+        boxEl.innerHTML = i;
+        colEL.append(boxEl);
+        console.log(boxEl);
+        rowElAp.append(colEL);
+        console.log(i);
+
+    } else {
+        //const colMarkUp = `<div class='col col-lg-2 col-xl py-3'><div class='box bg-i'>${i}</div></div>`;
+        const colEL = document.createElement('div');
+        const boxEl = document.createElement('div');
+        colEL.classList = 'col col-lg-2 col-xl py-3';
+        boxEl.classList = 'box bg-i';
+        boxEl.innerHTML = i;
+        colEL.append(boxEl);
+        console.log(boxEl);
+        rowElAp.append(colEL);
+        console.log(i);
+    };
+    
+
+}
